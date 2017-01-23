b400e05a984a72dcade267c56a8ab730a25d9bc9
Austin <Owstingreen@gmail.com>
2017-01-23 16:20:49 -0600

    added new script

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+function linker() {
+	var y = document.getElementsByClassName('alg');
+	var pref = 'https://alg.cubing.net/?type=alg&view=playback&alg=';
+	var algs = [];
+	var refs = [];
+	var i;
+	for(i = 0; i < y.length; i++) {
+		algs[i] = y[i].innerHTML.replace(/ /g, '_').replace(/'/g, '-');
+		refs[i] = pref + algs[i];
+		y[i].href = refs[i];
+	}
+};
