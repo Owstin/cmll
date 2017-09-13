bfe921013c99881db143c8cbb51161778d3824a0
Owstin <owstingreen@gmail.com>
2017-09-12 22:18:32 -0500

    cube images now turn

--- js/cmll.js
+++ js/cmll.js
@@ -1,8 +1,22 @@
 $(function() {
     $('.alg').hover(function() {
-        const card = $(this).parent().parent().children();//.parent().children()[0];
-        const img = $(card).find('img', this);
-        //$(card).toggleClass('flip');
-        console.log(img);
+        const direction = rotateDirection(this);
+        const img = $(this).parent().siblings('img');//.parent().children()[0];
+        img.toggleClass(direction);
     });
 });
+
+function rotateDirection(alg) {
+    var text = $(alg).text();
+    if(text.charAt(1) === 'U') {
+        switch(text.charAt(2)) {
+        case '\'':
+            return 'left';
+        case '2':
+            return 'double';
+        default:
+            return 'right';
+        }
+    }
+    return '';
+}
