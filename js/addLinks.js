d7dda9e4ccfa06ea29e42861e235de676af5b6ad
Owstin <owstingreen@gmail.com>
2017-09-13 23:00:41 -0500

    some styling things

--- js/addLinks.js
+++ js/addLinks.js
@@ -1,7 +1,8 @@
 $(function() {
     const url = 'https://alg.cubing.net/?type=alg&view=playback&alg=';
     $('.alg').each(function() {
-        const algText = $(this).text().replace(/ /g, '_').replace(/'/g, '-');
-        $(this).attr('href', url+algText);
+        const algText = $(this).text();
+        $(this).attr('href', url+algText.replace(/ /g, '_').replace(/'/g, '-'));
+        $(this).html(algText.replace(/ /g, '&nbsp'));
     });
 });
