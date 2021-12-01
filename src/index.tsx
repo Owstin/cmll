65a3f1f7db6c25ea008baab150789e9e388a8489
Austin Moore <owstingreen@gmail.com>
2021-11-30 19:35:29 -0600

    add some fancy vendor prefixing

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import 'pollen-css';
 import { render } from 'solid-js/web';
 import { MetaProvider } from 'solid-meta';
+import { setup } from 'solid-styled-components';
 
+import { prefixer } from './prefixer';
 import App from './App';
 import Head from './Head';
 import Styles from './Styles';
 import ThemeSwitcher from './ThemeSwitcher';
+
+setup(prefixer);
 
 render(
   () => (
