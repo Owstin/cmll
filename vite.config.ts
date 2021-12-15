1b0083beb1404db48ae9325f4a06ecbc09628de5
Austin Moore <owstingreen@gmail.com>
2021-12-15 12:13:15 -0600

    added some configuration to get routing working correctly with github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,36 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import legacy from '@vitejs/plugin-legacy';
+import { minifyHtml } from 'vite-plugin-html';
 
 export default defineConfig({
-  esbuild: false,
-  plugins: [solidPlugin(), legacy()],
+  assetsInclude: ['html'],
+  plugins: [
+    solidPlugin(),
+    legacy(),
+    minifyHtml({
+      collapseWhitespace: true,
+      decodeEntities: true,
+      minifyCSS: true,
+      minifyJS: true,
+      processConditionalComments: true,
+      removeAttributeQuotes: false,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      useShortDoctype: false,
+    }),
+  ],
   base: '/cmll/',
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
     emptyOutDir: true,
     assetsInlineLimit: 0,
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        '404': '404.html',
+      },
+    },
   },
 });
