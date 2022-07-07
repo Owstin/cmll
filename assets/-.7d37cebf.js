61ca5dd62cc3905ef60be79f8d5573785b5c4092
Austin Moore <owstingreen@gmail.com>
2022-07-06 23:14:18 -0500

    Updates

--- assets/-.7d37cebf.js
+++
@@ -1,1 +0,0 @@
-function t(){import("data:text/javascript,")}export{t as __vite_legacy_guard};
---
+++ assets/404.22077b0b.js
@@ -0,0 +1,4 @@
+import{s as e,a as t}from"./-.a3e1c7e3.js";const a=e.div`
+  text-align: center;
+  font-size: var(--scale-6);
+`,s=()=>t(a,{children:"There is nothing here"});export{s as default};
