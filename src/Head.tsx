dbd76185ba1a756743b64b2ef0427f3889671383
owstin <6243457+Owstin@users.noreply.github.com>
2026-04-20 13:11:40 -0500

    add simple nav bar

--- src/Head.tsx
+++ src/Head.tsx
@@ -1,12 +1,12 @@
 import { Component } from 'solid-js';
 import { Link, Title } from 'solid-meta';
 
-import { getAssetUrl } from './utils';
+import favicon from './assets/favicon.png?url';
 
 const Head: Component = () => (
   <>
     <Title>BigGreen's cool CMLL page</Title>
-    <Link rel="icon" type="image/png" href={getAssetUrl('favicon.png')} />
+    <Link rel="icon" type="image/png" href={favicon} />
   </>
 );
 
---
+++ src/Nav.tsx
@@ -0,0 +1,48 @@
+import { Component } from 'solid-js';
+import { styled } from 'solid-styled-components';
+
+import { useThemeSwitcher } from './ThemeSwitcher';
+import ThemeToggle from './ThemeToggle';
+
+const SitckyNav = styled('nav')`
+  position: sticky;
+  top: 0;
+  height: 48px;
+  width: 100%;
+  background-color: hsl(120deg, 50%, 30%);
+  z-index: 1;
+  box-shadow: var(--elevation-3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 var(--size-10);
+  width: 100%;
+
+  @media only screen and (min-width: 1440px) {
+    max-width: 1440px;
+  }
+`;
+
+const NavTitle = styled('h1')`
+  color: var(--text-color-white);
+`;
+
+const Nav: Component = () => {
+  const { switchTheme, isDarkTheme } = useThemeSwitcher();
+  return (
+    <SitckyNav>
+      <Content>
+        <NavTitle>BigGreen's Cool Algs</NavTitle>
+        <ThemeToggle toggleTheme={switchTheme} checked={!isDarkTheme()} />
+      </Content>
+    </SitckyNav>
+  );
+};
+
+export default Nav;
