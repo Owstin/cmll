dd8adbb757c967835da4fa8a917bbeeb159134da
owstin <6243457+Owstin@users.noreply.github.com>
2026-04-20 13:11:40 -0500

    did some theme stuff

--- src/Styles.tsx
+++ src/Styles.tsx
@@ -1,8 +1,16 @@
 import { createGlobalStyles as createGlobalStyle } from 'solid-styled-components';
 
+import { Theme } from './ThemeSwitcher';
+
+type Props = { theme: Theme };
+
 const Styles = createGlobalStyle`
   :root {
     --color-white: #fff;
+    --color-grey: hsl(0, 0%, 17%);
+    --color-light-grey: hsl(0, 0%, 25%);
+    --text-color-black: rgba(0, 0, 0, 0.87);
+    --text-color-white: rgba(255, 255, 255, 0.87);
   }
 
   html, body {
@@ -17,7 +25,8 @@ const Styles = createGlobalStyle`
   html {
     font-size: var(--size-4);
     width: var(--size-full);
-    background-color: var(--color-white);
+    background-color: ${(props: Props) => props.theme.backgroundColor};
+    color: ${(props: Props) => props.theme.textColor};
   }
 `;
 
