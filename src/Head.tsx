a33b51de2cdd6dbb31566571e0d52b30ee25ff34
owstin <6243457+Owstin@users.noreply.github.com>
2026-04-20 13:11:56 -0500

    threw pollen all over the project

--- src/Head.tsx
+++ src/Head.tsx
@@ -1,12 +1,12 @@
 import { Component } from 'solid-js';
 import { Link, Title } from 'solid-meta';
 
-import { getImageUrl } from './utils';
+import { getAssetUrl } from './utils';
 
 const Head: Component = () => (
   <>
     <Title>BigGreen's cool CMLL page</Title>
-    <Link rel="icon" type="image/png" href={getImageUrl('favicon.png')} />
+    <Link rel="icon" type="image/png" href={getAssetUrl('favicon.png')} />
   </>
 );
 
