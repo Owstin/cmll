242e3a75cb82d1281f35e65a437d05c7db43fa99
owstin <6243457+Owstin@users.noreply.github.com>
2026-04-20 13:11:56 -0500

    changed everything

---
+++ src/Head.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'solid-js';
+import { Link, Title } from 'solid-meta';
+
+const Head: Component = () => (
+  <>
+    <Title>BigGreen's cool CMLL page</Title>
+    <Link rel="icon" type="image/png" href="assets/favicon.png" />
+  </>
+);
+
+export default Head;
